0f7b46c4c4d3ed49243e71c4e235d221b94839bf
kaiquec3 <74798358+kaiquec3@users.noreply.github.com>
2020-11-20 20:09:18 -0300

    Add files via upload

---
+++ Exercício 3/Exercício 3/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercício_3
+{
+    class Quadrado
+    {
+        double diagonal;
+        double area;
+
+        public void SetDiagonal(double p)
+        {
+            diagonal = p;
+        }
+
+        public double GetDiagonal()
+        {
+            return diagonal;
+        }
+
+        public double GetArea()
+        {
+            return area;
+        }
+
+        public double CalcularArea()
+        {
+            area = Math.Pow(diagonal, 2)/2;
+            return area;
+        }
+    }
+}
---
+++ Exercício 3/Exercício 3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercício_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a1 = new Quadrado();
+            double k;
+
+            do
+            {
+                Console.Write("Informe o valor da diagonal do quadrado: ");
+                k = double.Parse(Console.ReadLine());
+
+                if (k > 0) a1.SetDiagonal(k);
+            }
+            while (k <= 0);
+            
+
+            Console.WriteLine($"O valor da área do quadrado é igual a {a1.CalcularArea()}");
+        }
+    }
+}
